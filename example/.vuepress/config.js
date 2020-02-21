5287c12c1e29cce39656dfed2120337b262c7088
Rain120 <chao1202016@outlook.com>
2020-02-21 22:45:25 +0800

    chore: build example

--- example/.vuepress/config.js
+++ example/.vuepress/config.js
@@ -19,7 +19,7 @@ module.exports = {
         }
       ],
     ],
-    base: './vuepress-plugin-awesome-gitalk/dist/',
+    base: '/dist/',
     title: 'Vuepress Plugin with Gitalk',
     head: [
       [
